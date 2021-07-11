919fa0c87942f0e34581daa846664fa73aedbedf
ericcoker18 <ericcoker18@Yahoo.com>
2021-07-11 14:27:33 -0500

    Commit #2

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { TeacherComponent } from './teacher/teacher.component';
+import { PersonComponent } from './person/person.component';
+import { PeopleService } from './services/people.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { TeacherComponent } from './teacher/teacher.component';
     CounterComponent,
     FetchDataComponent,
     StudentsComponent,
-    TeacherComponent
+    TeacherComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -35,7 +38,8 @@ import { TeacherComponent } from './teacher/teacher.component';
       { path: 'counter', component: CounterComponent },
         { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
         { path: 'students', component: StudentsComponent, canActivate: [AuthorizeGuard] },
-        { path: 'teachers', component: TeacherComponent, canActivate: [AuthorizeGuard] },
+      { path: 'teachers', component: TeacherComponent, canActivate: [AuthorizeGuard] },
+      { path: 'peoplelist', component: PersonComponent, canActivate: [AuthorizeGuard]},
     ])
   ],
   providers: [
---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using capstone.Data;
+using capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace capstone.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : Controller
+    {
+        private ApplicationDbContext _context;
+        public PeopleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<People> Get()
+        {
+            return _context.People.ToList();
+        } 
+
+        [HttpPost]
+        public People Post([FromBody]People person)
+        {
+            _context.People.Add(person);
+            _context.SaveChanges();
+            return person;
+                
+        }
+
+
+
+
+    }
+}
